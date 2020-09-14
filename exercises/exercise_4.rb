5df9ca48e9866e3899797d157290881642ba5bb0
Michele Barbiero <barbmich.since86@gmail.com>
2020-09-14 23:18:15 +0000

    add exercise 4 code

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,11 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_store = Store.where(mens_apparel: true)
+puts Store.count
+puts @mens_store.count
+# outputs 3 because store3 has been deleted
